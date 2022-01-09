900edf72c947587d7a133c1364713bf9abeb305e
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2022-01-09 07:47:19 -0300

    add Create Form

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -8,12 +8,23 @@ use Illuminate\Http\Request;
 class CursoController extends Controller
 {
     public function index(){
-        $cursos = curso::paginate(10);
+        $cursos = curso::orderBy('id','desc')->paginate(10);
         return view('cursos.index', compact('cursos'));
     }
 
     public function create(){
         return view('cursos.create');
+    }
+
+    public function store(Request $request){
+        $curso = new curso();
+
+        $curso->name = $request->name;
+        $curso->description = $request->description;
+        $curso->category = $request->category;
+
+        $curso->save();
+        return redirect()->route('cursos.show', $curso);
     }
 
     public function show($id){
